5d2daa0eec0d58e05db172f798907ea31e891457
노소래 <yessorae@gmail.com>
2026-02-25 02:47:33 +0900

    chore: vitest 테스트 환경 설정 및 계산 엔진 라이브러리 설치

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+// vitest.config.ts
+import path from 'path'
+
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    environment: 'node',
+    globals: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
+})
